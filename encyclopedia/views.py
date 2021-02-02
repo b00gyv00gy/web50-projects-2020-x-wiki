d9eae9dc7b3a207f0d7bff3c050f70048e498202
Anton Trushin <anton.trushin@mail.ru>
2021-02-02 16:49:09 +0300

    done search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from . import util
 
 
@@ -13,3 +15,31 @@ def get_entry(request, title):
         "title": title,
         "entry": util.get_entry(title)
     })
+
+    
+def search_result(request):
+
+    full_match = False
+    partial_matches = []
+
+    if request.method == 'GET':
+        title = request.GET['q']
+        for entry in util.list_entries():
+            if title.lower() == entry.lower():
+                full_match = True
+                break
+            elif title.lower() in entry.lower():
+                partial_matches.append(entry)
+            
+        if full_match:
+            return render(request, "encyclopedia/get_entry.html", {
+            "title": title,
+            "entry": util.get_entry(title)
+            })
+        elif partial_matches:
+            return render(request, "encyclopedia/index.html", {
+            "entries": partial_matches
+            })
+        else:
+            return render (request, "encyclopedia/search_result.html", {})
+
