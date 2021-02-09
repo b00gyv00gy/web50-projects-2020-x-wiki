fe83a79a6f13bb2a7990bfc48097c664f470ec1f
Anton Trushin <anton.trushin@mail.ru>
2021-02-09 15:55:26 +0300

    done all tasks

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.get_entry, name="get_entry"),
+    path("wiki/<str:title>", views.get_entry, name="get_entry"),
     path("search_result/", views.search_result, name="search_result"),
     path("new_page/", views.new_page, name="new_page"),
-    path("edit_page/", views.edit_page, name="edit_page"),
-    path("edit_entry/", views.edit_entry, name="edit_entry")
+    path("edit_page/<str:title>/", views.edit_page, name="edit_page"),
+    path("random_page/", views.random_page, name="random_page")
 ]
